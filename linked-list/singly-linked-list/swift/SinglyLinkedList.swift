43452b53c63fdbb6e9ec4ce8e52889c70d43f633
Blake Romero <codeblake@outlook.com>
2021-04-18 14:57:02 +0100

    Added count & first computed properties

--- linked-list/singly-linked-list/swift/SinglyLinkedList.swift
+++ linked-list/singly-linked-list/swift/SinglyLinkedList.swift
@@ -1,10 +1,13 @@
 class SinglyLinkedList {
     var head: Node
-    var length: Int = 0
+    private var length: Int = 0
 
     init(_ head: Node) {
         self.head = head
         length += 1
     }
+
+    var count: Int { length }
+    var first: Any { head.data }
 }
 
