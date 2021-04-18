9333378fdd03f58353922e1db3e0d69fe6b252d7
Blake Romero <codeblake@outlook.com>
2021-04-18 15:22:22 +0100

    Added description variable to print the list

--- linked-list/singly-linked-list/swift/SinglyLinkedList.swift
+++ linked-list/singly-linked-list/swift/SinglyLinkedList.swift
@@ -10,6 +10,19 @@ class SinglyLinkedList {
     var count: Int { length }
     var first: Any { head.data }
 
+    var description: String {
+        var string = "["
+        var currentNode = head
+        while currentNode.next != nil {
+            string.append("\(currentNode.data)")
+            currentNode = currentNode.next!
+            string += ","
+        }
+        string.append("\(currentNode.data)")
+        string += "]"
+        return string
+    }
+
     func prepend(_ data: Any) {
          // store head node
          let tempNode = head
