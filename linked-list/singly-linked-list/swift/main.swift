af717284c102865a4d3ff0b6d2afe01007b9f6a6
Blake Romero <codeblake@outlook.com>
2021-04-19 13:08:53 +0100

    Added Singly Linked List tests

---
+++ linked-list/singly-linked-list/swift/main.swift
@@ -0,0 +1,36 @@
+// ------------------------
+// Singly Linked List Tests
+// ------------------------
+
+// Create a Singly Linked List
+let myLinkedList = SinglyLinkedList(Node("hello"))
+
+// Get head data
+print(myLinkedList.head.data)
+
+// Get length of list
+print("Length: \(myLinkedList.count)")
+
+// Get first item
+print("1st item is: \(myLinkedList.first)")
+
+// Prepend data
+var data = "world"
+myLinkedList.prepend(data)
+print("Prepended: \(data)")
+
+// Print linked list
+print(myLinkedList.description)
+
+// Append Data
+data = "1111"
+myLinkedList.append(data)
+print("Appended: \(data)")
+print(myLinkedList.description)
+
+// Insert
+data = "33"
+var position = 2
+myLinkedList.insert(data, at: 2)
+print("Inserted \(data) at position \(position)")
+print(myLinkedList.description)
