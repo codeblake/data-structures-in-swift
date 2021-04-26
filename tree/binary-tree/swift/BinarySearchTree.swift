54670245cf559fbb41ca39308e0c38d71e009220
codeblake <codeblake@outlook.com>
2021-04-26 16:37:14 +0100

    Added BST Lookup function

--- tree/binary-tree/swift/BinarySearchTree.swift
+++ tree/binary-tree/swift/BinarySearchTree.swift
@@ -49,4 +49,31 @@ class BinarySearchTree {
         }
     }
 
+    /// Lookup - search for a value
+    func lookup(_ value: Int) -> Node? {
+        guard var node = root else {
+            return nil
+        }
+        while true {
+            // if found, return found node
+            if value == node.value{
+                return node
+            }
+
+            // go left if value less than current node
+            if value < node.value && node.left != nil {
+                node = node.left!
+                continue
+            }
+
+            // go right if value is greater-than current node
+            if value > node.value && node.right != nil {
+                node = node.right!
+                continue
+            }
+
+            // if nothing found return nil
+            return nil
+        }
+    }
 }
