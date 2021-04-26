07c745960bfaf7adf314d493f0e0e03db403563b
codeblake <codeblake@outlook.com>
2021-04-26 16:36:30 +0100

    Added BST insert function

--- tree/binary-tree/swift/BinarySearchTree.swift
+++ tree/binary-tree/swift/BinarySearchTree.swift
@@ -7,3 +7,46 @@ class Node {
         self.value = value
     }
 }
+
+class BinarySearchTree {
+    var root: Node?
+
+    init() { }
+
+    init(root: Int) {
+        self.root = Node(root)
+    }
+
+    /// Insert a value
+    func insert(_ value: Int) {
+        // when the tree is empty, insert the root node
+        guard var node = root else {
+            root = Node(value)
+            return
+        }
+
+        // Loop through the tree to find where to insert the new value
+        while true {
+            // if less than or equal go left
+            if value <= node.value {
+                // if left is nil insert new node
+                guard let left = node.left else {
+                    node.left = Node(value)
+                    return
+                }
+                node = left
+            }
+
+            // if greater than go right
+            if value > node.value {
+                // if right is nil insert new node
+                guard let right = node.right else {
+                    node.right = Node(value)
+                    return
+                }
+                node = right
+            }
+        }
+    }
+
+}
